12fcff757574ca1349f7444c3638539e771874bb
cui2018 <cxhui2015@163.com>
2019-06-17 14:18:25 +0800

    提交

--- cxf/src/main/java/com/cui/webservice/client/controller/ClientTest.java
+++ cxf/src/main/java/com/cui/webservice/client/controller/ClientTest.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.namespace.QName;
-import java.beans.PropertyDescriptor;
 
 /**
  * @Auther: cui
